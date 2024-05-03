57ffba94fe3a329fd3cd808c2dd3f9e0a1b2a19e
msopiann <muhammadsopian452@gmail.com>
2024-05-04 01:47:50 +0700

    feat: add boticon component

---
+++ src/components/BotIcon.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+export default function BotIcon() {
+  return (
+    <div
+      style={{
+        backgroundColor: "rgb(25, 195, 125)",
+      }}
+      className="relative p-1 rounded-full h-9 w-9 text-white flex items-center justify-center"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-bot"
+      >
+        <path d="M12 8V4H8" />
+        <rect width="16" height="12" x="4" y="8" rx="2" />
+        <path d="M2 14h2" />
+        <path d="M20 14h2" />
+        <path d="M15 13v2" />
+        <path d="M9 13v2" />
+      </svg>
+    </div>
+  );
+}
